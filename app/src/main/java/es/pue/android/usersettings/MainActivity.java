b699233d6d4f8058f813f81f415857b244150fb4
Manel <yo@manelperez.com>
2018-06-12 12:23:38 +0200

    Reordered design in main activity

--- app/src/main/java/es/pue/android/usersettings/MainActivity.java
+++ app/src/main/java/es/pue/android/usersettings/MainActivity.java
@@ -2,8 +2,12 @@ package es.pue.android.usersettings;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import es.pue.android.usersettings.fragments.SettingsButtonFragment;
 import es.pue.android.usersettings.fragments.SettingsFragment;
@@ -11,7 +15,6 @@ import es.pue.android.usersettings.fragments.SettingsFragment;
 public class MainActivity extends AppCompatActivity implements SettingsButtonFragment.ISettingsAccess {
 
     private FragmentManager fragmentManager;
-    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +22,24 @@ public class MainActivity extends AppCompatActivity implements SettingsButtonFra
         setContentView(R.layout.activity_main);
 
         fragmentManager = getFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.settings_container, new SettingsButtonFragment()).commit();
 
     }
 
     @Override
     public void showSettings() {
-        fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         // With this line, we return to activity on click in return button.
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.replace(R.id.settings_container, new SettingsFragment()).commit();
+    }
+
+    public void showVarsInSettings(View view) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String urlNews = prefs.getString("etUrlNews", "default_value_here");
+        boolean loadNews = prefs.getBoolean("shouldLoadNews", true);
+
+        Log.d("DEBUG", "showVarsInSettings: "+urlNews);
     }
 }

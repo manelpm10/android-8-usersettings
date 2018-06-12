769b3f8508701b8ff95bf89a78859014fd47fdbf
Manel <yo@manelperez.com>
2018-06-12 10:25:33 +0200

    Show settings fragment on open app

--- app/src/main/java/es/pue/android/usersettings/MainActivity.java
+++ app/src/main/java/es/pue/android/usersettings/MainActivity.java
@@ -1,13 +1,25 @@
 package es.pue.android.usersettings;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import es.pue.android.usersettings.fragments.SettingsFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.settings_container, new SettingsFragment()).commit();
+
     }
 }
